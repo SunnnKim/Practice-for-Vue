6dd3a18fa06d113aeedc48e865e49e9043e11593
Sun ju Kim <ksunju0610@naver.com>
2020-05-25 23:47:35 +0900

    Spring boot : Lombok

---
+++ Ex08_Lombok/src/main/java/com/study/springboot/Member.java
@@ -0,0 +1,14 @@
+package com.study.springboot;
+
+import lombok.Data;
+
+@Data		// Lombok을 사용하여 객체의 getter / setter 자동생성  
+public class Member {
+
+	// command 객체 : 파라미터와 관련됨 
+	// DTO : 데이터베이스 테이블과 같은 이름으로 구성된 객체 
+	
+	private String id;
+	private String name;
+	
+}
---
+++ Ex08_Lombok/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,35 @@
+package com.study.springboot;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception{
+		return "Lombok test";
+	}
+	
+	@RequestMapping("/test")
+	public String test1 (HttpServletRequest request, Model model) {
+		
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		
+		model.addAttribute("id", id);
+		model.addAttribute("name", name);
+		
+		return "index";
+	}
+	
+
+	
+	
+}
