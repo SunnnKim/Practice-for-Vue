354036e4569d0f27365b3539f64e34f0649f6f10
Sun ju Kim <ksunju0610@naver.com>
2020-07-14 13:37:19 +0900

    Inflearn JS : Prototype 1

--- frontend/egoing_node/main.js
+++ frontend/egoing_node/main.js
@@ -6,7 +6,7 @@ var app = http.createServer(function (request, response) {
     var _url = request.url;
 
     var queryData  = url.parse( _url, true).query;
-    console.log(queryData)
+    console.log(queryData.id)
 
 
     if (_url == '/') {
