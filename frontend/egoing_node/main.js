e174e552d39008c91ff1eea7367c772c3a7074d8
Sun ju Kim <ksunju0610@naver.com>
2020-07-15 14:58:20 +0900

    Inflearn JS : Class

--- frontend/egoing_node/main.js
+++ frontend/egoing_node/main.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring')
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
 
     var queryData  = url.parse( _url, true).query;
-    console.log(queryData.id)
 
 
     if (_url == '/') {
@@ -15,8 +15,25 @@ var app = http.createServer(function (request, response) {
     if (_url == '/favicon.ico') {
         return response.writeHead(404);
     }
+    if(_url == '/creat_process'){
+        var body = '';
+        request.on('data', function(data){
+            body += data
+            console.log(data)
+        })
+        request.on('end', function(){
+            var query = qs.parse(body)
+            console.log(query)
+        })
+        response.writeHead(200);
+        response.end('success');
+        return;
+    }
+else{
+
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + _url));
-
+    
+}
 });
 app.listen(3000);
